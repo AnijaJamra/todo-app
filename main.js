2fbffee9603824ab015a39e88d6eeec7f1f6c276
AnijaJamra <anijajamra@gmail.com>
2025-07-15 16:23:46 +0530

    added delete ,edit  functionality

--- main.js
+++ main.js
@@ -6,8 +6,36 @@ let ul = document.querySelector("ul");
 
 
 button.addEventListener("click",()=>{
- let li = document.createElement("li");
- li.innerText = input.value;
- ul.appendChild(li);
+
+
+if(input.value.trim()){
+   ul.innerHTML +=`
+   <li>
+   <span>${input.value}</span>
+   <div>
+   <button onclick="editTask(this)"> <i class="ri-edit-2-fill"></i> </button>
+   <button onclick="deleteTask(this)">  <i class="ri-delete-bin-6-fill"></i> </button>
+   </div>
+   </li>`
+}
  input.value ="";
 });
+
+
+function deleteTask(btn){
+   btn.closest("li").remove(); 
+};
+
+function editTask(btn){
+   const span = btn.closest("li").querySelector("span");
+   const newValue = prompt("edit Task:", span.textContent);
+   if(newValue !== null){
+      span.textContent = newValue.trim();
+   }
+};
+
+
+
+
+
+
